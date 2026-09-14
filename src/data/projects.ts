7a619a30ea21e042787c11021f730723a7ceeda6
Johnaoe <jonarbut@gmail.com>
2026-09-14 12:11:20 +0300

    habits

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,6 +5,16 @@ import gallery3 from '../assets/gallery-3.webp';
 import gallery4 from '../assets/gallery-4.webp';
 import gallery5 from '../assets/gallery-5.webp';
 import gallery6 from '../assets/gallery-6.webp';
+import gallery7 from '../assets/gallery-7.webp';
+import gallery8 from '../assets/gallery-8.webp';
+import gallery9 from '../assets/gallery-9.webp';
+import gallery10 from '../assets/gallery-10.webp';
+import gallery11 from '../assets/gallery-11.webp';
+import gallery12 from '../assets/gallery-12.webp';
+import gallery13 from '../assets/gallery-13.webp';
+import gallery14 from '../assets/gallery-14.webp';
+import gallery17 from '../assets/gallery-17.webp';
+import gallery19 from '../assets/gallery-19.webp';
 
 export interface Project {
   id: string;
@@ -15,5 +25,9 @@ export const projects: Project[] = [
   {
     id: 'connessioni2025',
     images: [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6]
+  },
+  {
+    id: 'healthyHabits2026',
+    images: [gallery7, gallery8, gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery17, gallery19]
   }
 ];
